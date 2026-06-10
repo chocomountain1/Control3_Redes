094aa86b18fe57f1845870b20ef28831590ee7e2
Camilo Ignacio Álvarez Quinteros <kmilo@MacBook-Air-de-Camilo.local>
2026-06-09 22:50:28 -0400

    Agregada la parte 1 sobre correr el código con argumentos con input de usuario

---
+++ Actividad 5/router.py
@@ -0,0 +1,9 @@
+import sys
+
+router_ip = sys.argv[1]
+router_puerto = int(sys.argv[2])
+router_rutas = sys.argv[3]
+
+print(f"Router IP: {router_ip}")
+print(f"Router Puerto: {router_puerto}")
+print(f"Router Rutas: {router_rutas}")
