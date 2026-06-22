147c6bae2f44749cce2d58e299d7f18b47f3c396
camilo <camilo.alvarez.q@ug.uchile.cl>
2026-06-22 14:35:25 -0400

    Algunos cambios a la actividad de forwarding con ttl

--- Actividad 5/test_check_rutas.py
+++
@@ -1,8 +0,0 @@
-from checkear_rutas import check_routes
-
-print (check_routes("rutas_R1.txt", ("127.0.0.1", 8882))) # ('127.0.0.1', 8882)
-print (check_routes("rutas_R1.txt", ("127.0.0.1", 8884))) # None
-print (check_routes("rutas_R2.txt", ("127.0.0.1", 8881))) # ('127.0.0.1', 8881)
-print (check_routes("rutas_R2.txt", ("127.0.0.1", 8883))) # ('127.0.0.1', 8883)
-print (check_routes("rutas_R2.txt", ("127.0.0.1", 8884))) # None
-print (check_routes("rutas_R3.txt", ("127.0.0.1", 8882))) # ('127.0.0.1', 8882)
